b890e182f1ebede1f21a17817f9e48a929361b06
parivahana <parivahana@protonmail.com>
2022-05-06 14:51:52 +0530

    adding files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import "bulma/css/bulma.min.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="has-text-primary">
+        Coming Soon
+      </h1>
+    </>
+  )
+}
+
+export default App
