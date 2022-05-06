c78329c81f650f38872d2d57b59b6e43fa897fc1
parivahana <parivahana@protonmail.com>
2022-05-06 19:02:24 +0530

    base files

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "bulma/css/bulma.min.css";
 function App() {
   return (
     <>
-      <h1 className="has-text-primary">
+      <h1 className="has-text-centered is-size-1">
         Coming Soon
       </h1>
     </>
